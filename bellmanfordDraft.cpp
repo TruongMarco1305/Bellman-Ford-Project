245517c5016f82b44ec9a9f8002abee534048e94
TruongMarco1305 <tgknam2005@gmail.com>
2024-04-19 11:44:37 +0700

    bla bla

--- bellmanfordDraft.cpp
+++ bellmanfordDraft.cpp
@@ -6,26 +6,33 @@ using namespace std;
 int main(){
     int vertices, edges, starting, end;
     cin >> vertices >> edges >> starting >> end;
-    int ** weightGraph = new int*[edges];
+    int weightGraph[100][100];
     for (int i = 0; i < vertices; i ++){
         for (int j = 0; j < vertices; j++){
-            weightGraph[i][j] = 0;
+            weightGraph[i][j] = -1;
         }
     }
-    for (int i = 0; i < vertices; i ++){
-        for (int j = 0; j < vertices; j++){
-            cout << weightGraph[i][j];
-        }
-    }
-    /*int a,b,c;
+
+    int a,b,c;
     for (int i = 0; i < edges; i++){
         cin >> a >> b >> c;
         weightGraph[a - 1][b - 1] = c; 
-    }*/
-
-    for (int i = 0; i < edges; i++){
-        delete weightGraph[i];
     }
-    delete weightGraph;
+    for (int i = 0; i < vertices; i ++){
+        for (int j = 0; j < vertices; j++){
+            cout << weightGraph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int arr[100] = {0};
+    for (int i = 0; i < )
+    int relaxedTime = 0;
+    while (relaxedTime < vertices - 1){
+
+    }
+
+    int distance = INT_MAX;
+    
     return 0;
 }
