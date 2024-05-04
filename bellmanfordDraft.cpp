ff64bfeee90d5ce91bb181ceb11795baa3cf737a
TruongMarco1305 <tgknam2005@gmail.com>
2024-05-04 11:18:52 +0700

    adding

--- bellmanfordDraft.cpp
+++ bellmanfordDraft.cpp
@@ -3,30 +3,37 @@
 
 using namespace std;
 
-void initCityMap(int & numCities, int **map){
+void initCityMap(int & numCities, int ** map){
     cout << "Input the number of city: ";
     cin >> numCities;
     for (int i = 0; i < numCities; i++){
         map[i] = new int[numCities];
     }
-    int initial = 0;
-    for (int i = 0; i < numCities; i++){
-        for (int j = 0; j < numCities; j++){
-            map[i][j] = initial;
-        }
-    }
 
     for (int i = 0; i < numCities; i++){
         for (int j = 0; j < numCities; j++){
             if (i == j){
-                continue;
+                map[i][j] = 0;
             }else{
                 cout << "Input distance from city " << i + 1 << " to city " << j + 1 << ": ";
-                cin >> initial;
-                map[i][j] = initial;
-                map[j][i] = initial;
+                cin >> map[i][j];
             }
         } 
+    }
+}
+
+void relaxation(int*distance,int*prev,int**map,int numCities){
+    for (int u = 0; u < numCities; u++){
+        for (int v = 0; v < numCities; v ++){
+            if (map[u][v] == 0){
+                continue;
+            } else {
+                if (distance[u] + map[u][v] < distance[v]){
+                    distance[v] = distance[u] + map[u][v];
+                    prev[v] = u + 1;
+                }
+            }
+        }
     }
 }
 
@@ -40,9 +47,6 @@ int main(){
     int source;
     cout << "Input the source city: ";
     cin >> source;
-    int finish;
-    cout << "Input the finish city: ";
-    cin >> finish;
     for (int i = 0; i < numCities; i++){
         distance[i] = 1000;
         prev[i] = -1;
@@ -50,18 +54,7 @@ int main(){
     distance[source - 1] = 0;
     int relaxTime = 0;
     while (relaxTime < numCities - 1){
-        for (int u = 0; u < numCities; u++){
-            for (int v = 0; v < numCities; v ++){
-                if (map[u][v] == 0){
-                    continue;
-                } else {
-                    if (distance[u] + map[u][v] < distance[v]){
-                        distance[v] = distance[u] + map[u][v];
-                        prev[v] = u + 1;
-                    }
-                }
-            }
-        }
+        relaxation(distance,prev,map,numCities);
         relaxTime ++;
     }
     bool negativeCycle = false;
@@ -80,19 +73,26 @@ int main(){
         cout << "Error: Contains circuit of negative weight";
     } else{
         stack<int> path;
-        path.push(finish);
-        int length = distance[finish - 1];
-        while (finish != source){
-            path.push(prev[finish-1]);
-            finish = prev[finish-1];
-        }
-        cout << "The shortest distance from source city to finish city: " << length << endl;
-        cout << "The path from source city to finish city with shortest distance: " << endl;
-        cout << path.top();
-        while (!path.empty()){
-            path.pop();
-            if (path.empty()) break;
-            cout << "->" << path.top();
+        int finish = source;
+        int tmp;
+        while (finish <= numCities){
+            path.push(finish);
+            int length = distance[finish - 1];
+            tmp = finish;
+            while (tmp != source){
+                path.push(prev[tmp-1]);
+                tmp = prev[tmp-1];
+            }
+            cout << "The shortest distance from source city to city " << finish << ": " << length << endl;
+            cout << "The path from source city to city " << finish << " with shortest distance: ";
+            cout << path.top();
+            while (!path.empty()){
+                path.pop();
+                if (path.empty()) break;
+                cout << "->" << path.top();
+            }
+            cout << "\n";
+            finish ++;
         }
     }
     delete distance;
